bcf81864252cde2df111e9644e89f025eca7b255
ElliotVallentine <marianoferreira.fbs@gmail.com>
2022-12-27 22:59:37 -0300

    feature

--- guessingGame.c
+++ guessingGame.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main()
 {
+
+    const int NumeroDeTentativas = 3;
 
     printf("***************************************\n");
     printf("* Boas-Vindas ao jogo da adivinhação! *\n");
@@ -10,23 +14,42 @@ int main()
     int numerosecreto = 42;
     int chute;
 
-    printf("chute um número: ");
-    scanf("%i", &chute);
-    printf("Seu chute foi %i\n", chute);
-    printf("------------------\n");
+    for (int n = 1; n <= NumeroDeTentativas; n++)
+    {
 
-    int acerto = chute == numerosecreto;
-    
-    if (acerto)
-    {
-        printf("Parabéns! Você adivinhou o número secreto!\n");
+        printf("chute um número: ");
+        scanf("%i", &chute);
+        printf("Seu chute foi %i\n", chute);
+        printf("------------------\n");
+
+        int acerto = chute == numerosecreto;
+        int maior = chute > numerosecreto;
+        int menor = chute < numerosecreto;
+        
+        if (chute < 0)
+        {
+            printf("Você não pode chutar números negativos\n");
+            printf("------------------\n");
+            n--;
+            continue;
+        }
+        else if (acerto)
+        {
+            printf("Parabéns! Você adivinhou o número secreto!\n");
+            printf("------------------\n");
+            exit(0);
+        }
+        else if (maior)
+        {
+            printf("Você chutou um número maior que o número secreto.\n");
+            printf("------------------\n");
+        }
+        else if (menor)
+        {
+            printf("Você chutou um número menor que o número secreto.\n");
+            printf("------------------\n");
+        }
     }
-    else if (chute > numerosecreto)
-    {
-        printf("Você chutou um número maior que o número secreto.\n");
-    }
-    else if (chute < numerosecreto)
-    {
-        printf("Você chutou um número menor que o número secreto.\n");
-    }
+
+    printf("fim do programa\n");
 }
